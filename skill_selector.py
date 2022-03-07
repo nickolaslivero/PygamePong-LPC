78592b8ae781fe5aeede6e50dfb06af7b4b979b1
BEATRIZ DAMASCENO FERNANDES <93746097+beatrizdamascenof@users.noreply.github.com>
2022-03-07 17:34:51 -0400

    randomização da seleção de skills

--- skill_selector.py
+++ skill_selector.py
@@ -1,23 +1,34 @@
 import random
-
+ 
 # from config import *
 import player
 from player import *
-
-
+from config import *
+ 
+ 
 class Skill(pygame.sprite.Sprite):
     def __init__(self):
         global vector
-        vector = ['img/slow_opponent.png', 'img/slow_yourself.png', 'img/bigger_opponent.png',
-                  'img/bigger_yourself.png', 'img/freeze_opponent.png', 'img/freeze_yourself.png',
-                  'img/invert_opponent.png', 'img/invert_yourself.png', 'img/smaller_opponent.png',
-                  'img/smaller_yourself.png']
+        vector = ['img/slow_opponent.png', 
+                  'img/freeze_opponent.png', 
+                 ]
         pygame.sprite.Sprite.__init__(self)
         self.position_x = 450
         self.position_y = 300
-
+        self.active = False
+        self.name = ""
+        self.player = 0
+ 
         self.image = pygame.image.load('{}'.format(random.choice(vector)))
-
-    def render(self, surface):
-        a = pygame.image.load('{}'.format(random.choice(vector)))
-        surface.blit(a, (self.position_x, self.position_y))
+ 
+    def render(self, surface, skill):
+        surface.blit(skill.image, (self.position_x, self.position_y))
+    
+    def set_image(self, skill):
+        string_vetor = random.choice(vector)
+        if string_vetor == 'img/slow_opponent.png':
+           skill.name = "slow"
+        else:
+            skill.name = "freeze"
+        skill.image = pygame.image.load('{}'.format(string_vetor))
+ 
